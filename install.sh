5d50689c9197022716e115863d70476f0b691f1f
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2020-07-16 16:04:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-apt install git
 apt install python
 apt install python3.8
-sudo rm /usr/bin/python && sudo ln -s python3.8 /usr/bin/python
+sudo rm $PREFIX/bin/python && sudo ln -s python3.8 $PREFIX/bin/python
 apt install python-pip
 pip install colorama
 pip install proxyscrape
