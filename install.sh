063d5a18c0bb3c25fe09837a109bbe396b96b325
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2020-07-16 16:02:12 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+apt install git
+apt install python
+apt install python3.8
+sudo rm /usr/bin/python && sudo ln -s python3.8 /usr/bin/python
+apt install python-pip
+pip install colorama
+pip install proxyscrape
+pip install requests
