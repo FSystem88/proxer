0951c34c6dbdff1a6d0c8db95294748c3ed07bba
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2020-10-30 00:00:10 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-apt install python
-apt install python3.8
-sudo rm $PREFIX/bin/python && sudo ln -s python3.8 $PREFIX/bin/python
-apt install python-pip
-pip install colorama
-pip install proxyscrape
-pip install requests
