2282f59d49e274f6037edc55a18b945c18a6b4b2
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2020-11-06 01:09:42 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,39 +1,28 @@
 #!/usr/bin/python3
-import os
-def MAIN():
+import requests as r, os, threading
+from threading import Thread
+from colorama import Fore,Style
+
+def check(ip, prox):
 	try:
-		import requests as r, os, threading
-		from threading import Thread
-		from colorama import Fore,Style
+		ipx = r.get("http://v4.ident.me/", proxies={'http':prox, 'https':prox}, verify=False, timeout=10).text
+	except:
+		ipx = ip
+	if ip != ipx:
+		print(Fore.GREEN+"{} good!".format(prox))
+		f = open("proxies.txt", "a+")
+		f.write("{}\n".format(prox))
+		f.close()
+	else:
+		print(Fore.RED+"{} bad".format(prox))
 
-		def check(ip, prox):
-			try:
-				ipx = r.get("http://v4.ident.me/", proxies={'http':prox, 'https':prox}, verify=False, timeout=10).text
-			except:
-				ipx = ip
-			if ip != ipx:
-				print(Fore.GREEN+"{} good!".format(prox))
-				f = open("proxies.txt", "a+")
-				f.write("{}\n".format(prox))
-				f.close()
-			else:
-				print(Fore.RED+"{} bad".format(prox))
-
-		url = "https://api.proxyscrape.com/?request=displayproxies&proxytype=http"
-		req = r.get(url)
-		ip = r.post("http://v4.ident.me/").text
-		array = req.text.split()
-		print(Fore.LIGHTYELLOW_EX+"Your ip: {}".format(ip)+Style.RESET_ALL)
-		for prox in array:
-			thread_list = []
-			t = threading.Thread (target=check, args=(ip, prox))
-			thread_list.append(t)
-			t.start()
-
-	except ModuleNotFoundError:
-		os.system('cls' if os.name=='nt' else 'clear')
-		input("Press ENTER to install missing libraries...")
-		os.system("apt install libxml2 libxslt -y")
-		os.system("python -m pip install requests bs4 colorama lxml")		
-		MAIN()
-MAIN()
+url = "https://api.proxyscrape.com/?request=displayproxies&proxytype=http"
+req = r.get(url)
+ip = r.post("http://v4.ident.me/").text
+array = req.text.split()
+print(Fore.LIGHTYELLOW_EX+"Your ip: {}".format(ip)+Style.RESET_ALL)
+for prox in array:
+	thread_list = []
+	t = threading.Thread (target=check, args=(ip, prox))
+	thread_list.append(t)
+	t.start()
