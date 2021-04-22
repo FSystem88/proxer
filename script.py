e35313f8f5d043603f7cf21cb46f41b4bfcd9b96
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2021-04-22 03:56:55 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,14 +1,24 @@
-#!/usr/bin/python
-import requests as r, os, threading
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Dev: FSystem88
+
+import requests as r, os, threading, random
 from threading import Thread
-from colorama import Fore,Style
+from colorama import Fore,Style,Back
 
 def clear():
 	os.system('cls' if os.name=='nt' else 'clear')
 
-def check(ip, prox):
+def logo():
+	colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.CYAN, Fore.MAGENTA, Fore.WHITE]
+	color1 = random.choice(colors)
+	colors.remove(color1)
+	color2 = random.choice(colors)
+	print(color1+"████████████████████████████████████\n█▄─▄▄─█▄─▄▄▀█─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█\n██─▄▄▄██─▄─▄█─██─██▀─▀███─▄█▀██─▄─▄█\n█▄▄▄███▄▄█▄▄█▄▄▄▄█▄▄█▄▄█▄▄▄▄▄█▄▄█▄▄█\n███████████"+color2+" by FSystem88 "+color1+"███████████\n████████████████████████████████████\n"+Style.RESET_ALL)
+
+def check(ip, prox, qtime):
 	try:
-		ipx = r.get("http://fsystem88.ru/ip", proxies={'http':'http://{}'.format(prox), 'https':'http://{}'.format(prox)}, timeout=10).text
+		ipx = r.get("http://fsystem88.ru/ip", proxies={'http':'http://{}'.format(prox), 'https':'http://{}'.format(prox)}, timeout=qtime).text
 	except:
 		ipx = ip
 	if ip != ipx:
@@ -19,15 +29,22 @@ def check(ip, prox):
 	else:
 		print(Fore.RED+"{} bad".format(prox))
 
-url = "https://api.proxyscrape.com/?request=displayproxies&proxytype=http"
-req = r.get(url)
-ip = r.post("http://fsystem88.ru/ip").text
-array = req.text.split()
 clear()
-print(Fore.LIGHTYELLOW_EX+"Your ip: {}".format(ip)+Style.RESET_ALL)
+logo()
+try:
+	qtime = int(input("Timeout proxy [seconds] (0 - all): "))
+	if qtime == 0:
+		qtime = None
+except:
+	print(Fore.RED+"\nIncorrect timeout proxy\n")
+	exit()
+req = r.get("https://api.proxyscrape.com/?request=displayproxies&proxytype=http")
+array = req.text.split()
+ip = r.post("http://fsystem88.ru/ip").text
+print(Back.GREEN+"Your ip: {}\n".format(ip)+Style.RESET_ALL)
 open("proxies.txt", "w+").close()
 for prox in array:
 	thread_list = []
-	t = threading.Thread (target=check, args=(ip, prox))
+	t = threading.Thread (target=check, args=(ip, prox, qtime))
 	thread_list.append(t)
 	t.start()
